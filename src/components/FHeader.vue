8d26758c1d8c7178fc3231c72d552c1a3bd3c42b
Youcef <youcefcopy@gmail.com>
2026-02-11 09:19:43 +0100

    navbar styled | logo && list

--- src/components/FHeader.vue
+++ src/components/FHeader.vue
@@ -2,7 +2,7 @@
 
 <template>
   <nav>
-    <h2>F&M</h2>
+    <h2 class="logo">F&M</h2>
     <ul>
       <li><a href="#">Products</a></li>
       <li><a href="#">Women</a></li>
@@ -14,4 +14,33 @@
   </nav>
 </template>
 
-<style scoped></style>
+<style scoped>
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 2rem;
+}
+
+.logo {
+  color: var(--brand-color);
+  font-size: clamp(1.3rem, 4vw + 1rem, 2.1rem);
+  font-weight: 900;
+}
+
+ul {
+  list-style-type: none;
+  display: inline-flex;
+  gap: 1rem;
+}
+
+a {
+  text-decoration: none;
+  color: var(--text-color);
+  transition: all 0.6s;
+}
+
+a:hover {
+  color: var(--brand-color);
+}
+</style>
