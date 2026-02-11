3673677f36c59b14596a5da3978f6ae1792460ca
Youcef <youcefcopy@gmail.com>
2026-02-11 10:02:46 +0100

    the video has been styled

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,10 +1,22 @@
 <script setup></script>
 
 <template>
-  <video controls autoplay loop>
-    <source src="/videos/hero.mp4" type="video/mp4" />
-    Your browser does not support the video tag.
-  </video>
+  <div class="vid-container">
+    <video autoplay muted loop>
+      <source src="/videos/hero.mp4" type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.vid-container {
+  height: calc(100vh - 81px);
+  width: 100%;
+}
+
+video {
+  width: 100%;
+  height: 100%;
+}
+</style>
