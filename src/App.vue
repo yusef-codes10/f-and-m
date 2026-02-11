3c08125ba7ffd66f9e3aba8892d5b75bd4585957
Youcef <youcefcopy@gmail.com>
2026-02-11 09:58:41 +0100

    adding a hero section video

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import FHeader from './components/FHeader.vue'
+import HeroSection from './components/HeroSection.vue'
 </script>
 
 <template>
   <FHeader />
+  <HeroSection />
 </template>
 
 <style scoped></style>
---
+++ src/components/HeroSection.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  <video controls autoplay loop>
+    <source src="/videos/hero.mp4" type="video/mp4" />
+    Your browser does not support the video tag.
+  </video>
+</template>
+
+<style scoped></style>
