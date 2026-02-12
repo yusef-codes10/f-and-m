497c5304426fb54ae1dd8b5ba1adabb9cdf8222a
Youcef <youcefcopy@gmail.com>
2026-02-12 18:10:56 +0100

    perfect grid layout for the footer

--- src/components/FFooter.vue
+++ src/components/FFooter.vue
@@ -1,5 +1,52 @@
 <script setup></script>
 
-<template>This is the footer</template>
+<template>
+  <footer>
+    <div class="help"></div>
+    <div class="services"></div>
+    <div class="about"></div>
+    <div class="legal"></div>
+    <div class="links"></div>
+    <div class="logo"></div>
+  </footer>
+</template>
 
-<style scoped></style>
+<style scoped>
+footer {
+  height: 100vh;
+  background-color: seagreen;
+
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-areas:
+    'help services about legal'
+    'links links links links'
+    'logo logo logo logo';
+}
+
+.help {
+  background-color: red;
+  grid-area: help;
+}
+
+.services {
+  background-color: green;
+  grid-area: services;
+}
+.about {
+  background-color: yellow;
+  grid-area: about;
+}
+.legal {
+  background-color: blue;
+  grid-area: legal;
+}
+.links {
+  background-color: blueviolet;
+  grid-area: links;
+}
+.logo {
+  background-color: tomato;
+  grid-area: logo;
+}
+</style>
