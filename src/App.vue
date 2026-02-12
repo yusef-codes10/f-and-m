3e0d228f9f866a8d7a4f3338f71a158dc24ca9ee
Youcef <youcefcopy@gmail.com>
2026-02-12 13:11:38 +0100

    preparing to build the footer

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import FHeader from './components/FHeader.vue'
 import HeroSection from './components/HeroSection.vue'
 import ProducsContainer from './components/ProducsContainer.vue'
+import FFooter from './components/FFooter.vue'
 </script>
 
 <template>
   <FHeader />
   <HeroSection />
   <ProducsContainer />
+  <FFooter />
 </template>
 
 <style scoped></style>
---
+++ src/components/FFooter.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>This is the footer</template>
+
+<style scoped></style>
