8c31a6176b6cea21ce5b2b764630701eaee8bc87
Youcef <youcefcopy@gmail.com>
2026-02-12 19:10:42 +0100

    footer content & toggle btn component

--- src/components/FFooter.vue
+++ src/components/FFooter.vue
@@ -1,12 +1,48 @@
-<script setup></script>
+<script setup>
+import ToggleBtn from './ToggleBtn.vue'
+</script>
 
 <template>
   <footer>
-    <div class="help"></div>
-    <div class="services"></div>
-    <div class="about"></div>
-    <div class="legal"></div>
-    <div class="links"></div>
+    <div class="help">
+      <h3>help</h3>
+      <ul>
+        <li>Contact us</li>
+        <li>Returns & Exchanges Conditions</li>
+        <li>Guest order tracking & Returns</li>
+        <li>Find a Boutique</li>
+        <li>FAQ</li>
+      </ul>
+    </div>
+    <div class="services">
+      <h3>services</h3>
+      <ul>
+        <li>Book an Apoitement</li>
+        <li>Product care</li>
+      </ul>
+    </div>
+    <div class="about">
+      <h3>about</h3>
+      <ul>
+        <li>Work with us</li>
+      </ul>
+    </div>
+    <div class="legal">
+      <h3>legal</h3>
+      <ul>
+        <li>Terms of Sale</li>
+        <li>Terms of use</li>
+        <li>Privacy policy</li>
+        <li>Accessability statement</li>
+        <li>Site map</li>
+        <li>Stop fake</li>
+        <li>US MSA statement</li>
+        <li>Credits</li>
+      </ul>
+    </div>
+    <div class="links">
+      <ToggleBtn />
+    </div>
     <div class="logo"></div>
   </footer>
 </template>
@@ -18,6 +54,7 @@ footer {
 
   display: grid;
   grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: 2fr auto auto;
   grid-template-areas:
     'help services about legal'
     'links links links links'
@@ -48,5 +85,17 @@ footer {
 .logo {
   background-color: tomato;
   grid-area: logo;
+}
+.help,
+.services,
+.about,
+.legal,
+.links,
+.logo {
+  padding: 2rem;
+}
+
+ul {
+  list-style-type: none;
 }
 </style>
---
+++ src/components/ToggleBtn.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { ref } from 'vue'
+const isOn = ref(false)
+</script>
+
+<template>
+  <label class="switch">
+    <input type="checkbox" v-model="isOn" />
+    <span class="slider"></span>
+  </label>
+
+  <p>{{ isOn }}</p>
+</template>
+
+<style>
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 50px;
+  height: 25px;
+}
+
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  background-color: #ccc;
+  border-radius: 25px;
+  inset: 0;
+  transition: 0.3s;
+}
+
+.slider::before {
+  content: '';
+  position: absolute;
+  height: 19px;
+  width: 19px;
+  left: 3px;
+  top: 3px;
+  background: white;
+  border-radius: 50%;
+  transition: 0.3s;
+}
+
+input:checked + .slider {
+  background-color: #4caf50;
+}
+
+input:checked + .slider::before {
+  transform: translateX(25px);
+}
+</style>
