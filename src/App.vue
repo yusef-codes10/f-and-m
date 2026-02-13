b58342819dbee731e2f5bf3715dfab0a5f0c3fed
Youcef <youcefcopy@gmail.com>
2026-02-13 10:01:02 +0100

    publish section is simialr to the hero section?

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@ import FHeader from './components/FHeader.vue'
 import HeroSection from './components/HeroSection.vue'
 import ProducsContainer from './components/ProducsContainer.vue'
 import FFooter from './components/FFooter.vue'
+import PublishSection from './components/PublishSection.vue'
 </script>
 
 <template>
   <FHeader />
   <HeroSection />
   <ProducsContainer />
+  <PublishSection />
   <FFooter />
 </template>
 
--- src/components/PublishSection.vue
+++ src/components/PublishSection.vue
@@ -1,5 +1,54 @@
 <script setup></script>
 
-<template><p></p></template>
+<template>
+  <div class="cta-section">
+    <p>Elegance in Motion</p>
+    <h1>CASSIA COLLECTION</h1>
+    <div class="discover-btn">
+      <button>DISCOVER</button>
+    </div>
+  </div>
+</template>
 
-<style scoped></style>
+<style scoped>
+.cta-section {
+  padding: 2rem 4rem;
+  /* border: 2px solid red; */
+  /* text-align: center; */
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+
+.cta-section h1 {
+  font-size: clamp(1.3rem, 4vw + 1rem, 2.8rem);
+  font-weight: 900;
+  color: var(--brand-color);
+}
+
+.cta-section p {
+  text-align: center;
+  color: var(--brand-color);
+}
+
+.discover-btn {
+  display: flex;
+  justify-content: center;
+}
+.discover-btn button {
+  background-color: transparent;
+  border-color: var(--bg-color);
+  border: 2px solid;
+  padding: 12px 16px;
+  cursor: pointer;
+  color: var(--brand-color);
+  font-size: 1.2rem;
+  font-weight: 600;
+  transform: all 0.6s;
+}
+
+.discover-btn button:hover {
+  background-color: var(--text-color);
+  color: var(--bg-color);
+}
+</style>
