047a5bb5b9640c37ce0990a9e2ba017b9332dbd2
Youcef <youcefcopy@gmail.com>
2026-02-17 11:33:48 +0100

    activeFilter computed property

--- src/stores/productStore.js
+++ src/stores/productStore.js
@@ -1,10 +1,18 @@
 // the pinia store for products
 import { defineStore } from 'pinia'
 import womenShoes from '@/data/women-shoes.js'
+import { computed } from 'vue'
 
 export const productsStore = defineStore('product', () => {
   // * state
   const shoes = womenShoes
+
+  //  * getters
+  //   favorite filter (computed)
+  const activeFilter = computed(() => {
+    shoes = shoes.filter((shoe) => shoe.isFavorite === true)
+    return shoes
+  })
 
   return {
     shoes,
