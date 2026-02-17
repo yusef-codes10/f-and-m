02eb0c5af04cd4a5c5c44c2b99eae87df0df7493
Youcef <youcefcopy@gmail.com>
2026-02-17 10:49:21 +0100

    installing & setting up pinia in main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createPinia } from 'pinia'
 import '@/assets/main.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+const pinia = createPinia()
+
+app.use(pinia)
+
+app.mount('#app')
---
+++ src/stores/productStore.js
@@ -0,0 +1,1 @@
+// the pinia store for products
