ad90d5fcd6385e168196127c027005a02408330c
Youcef <youcefcopy@gmail.com>
2026-02-19 13:18:58 +0100

    components in the HomeView instead of the app.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,1 +1,14 @@
 <!-- the main home vue '/' -->
+<script setup>
+import HeroSection from '@/components/HeroSection.vue'
+import ProducsContainer from '@/components/ProducsContainer.vue'
+import PublishSection from '@/components/PublishSection.vue'
+</script>
+
+<template>
+  <HeroSection />
+  <ProducsContainer />
+  <PublishSection />
+</template>
+
+<style scoped></style>
