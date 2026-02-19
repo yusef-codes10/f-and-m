4e8c618c62153212a871c6c113c28f63276eeb6f
Youcef <youcefcopy@gmail.com>
2026-02-19 13:22:00 +0100

    creating the routes objects

--- src/router/index.js
+++ src/router/index.js
@@ -1,2 +1,12 @@
 // this is the router file
+import HomeView from '@/views/HomeView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+  },
+  {},
+]
