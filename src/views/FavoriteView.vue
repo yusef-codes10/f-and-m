282cd16625eefa4a61f8255e778e5eb74a91f0e5
Youcef <youcefcopy@gmail.com>
2026-02-22 11:00:33 +0100

    preparing to load the favorite products only to the favorite view... import the pinia strore and the component

--- src/views/FavoriteView.vue
+++ src/views/FavoriteView.vue
@@ -1,5 +1,20 @@
+<script setup>
+import FProduct from '@/components/FProduct.vue'
+
+import { productsStore } from '@/stores/productStore'
+
+// we have to set the store
+const myStore = productsStore()
+console.log(myStore.shoes)
+</script>
+
 <template>
-  <div class="favorite">This is the favorite products</div>
+  <div class="favorite">
+    This is the favorite products
+    <h3>The favorite products</h3>
+    <FProduct />
+    <FProduct v-for="shoe in myStore.activeFilter" :key="shoe.id" :product="shoe" />
+  </div>
 </template>
 
 <style scoped>
