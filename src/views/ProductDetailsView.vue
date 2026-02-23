c603473dfe3967e52afeb3cf5fe55caccde6813d
Youcef <youcefcopy@gmail.com>
2026-02-23 11:00:05 +0100

    accessing the route params directly and finding the exact shoe by slug from the pinia store

--- src/views/ProductDetailsView.vue
+++ src/views/ProductDetailsView.vue
@@ -7,12 +7,22 @@ import { useRoute } from 'vue-router'
 
 const route = useRoute()
 console.log(route.params.slug)
+
+// importing the pinia store
+import { productsStore } from '@/stores/productStore'
+
+// we have to set the store
+const myStore = productsStore()
+console.log(myStore.shoes)
+
+const findTheProduct = () => myStore.shoes.find((shoe) => shoe.slug === route.params.slug)
+console.log(findTheProduct)
 </script>
 <template>
   <div class="details">
     <h1>Product details</h1>
     <!-- here we just access the url slug -->
-    <p>Your product is:</p>
+    <p>Your product is: {{ findTheProduct().title }}</p>
   </div>
 </template>
 <style scoped>
