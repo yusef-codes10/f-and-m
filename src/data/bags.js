d23a46d389b8fcc4dd0e45282d04b0fb6433362d
Yusef Codes <youcefcopy@gmail.com>
2026-02-26 15:42:03 +0100

    first bag data have been added

--- src/data/bags.js
+++ src/data/bags.js
@@ -1,1 +1,20 @@
 // bags array is going to be here
+const bags = [
+  {
+    id: 1,
+    preview: [
+      { id: 1, url: '/images/eva.jpg' },
+      { id: 2, url: '/images/eva-full.jpg' },
+      { id: 3, url: '/images/eva-top.jpg' },
+    ],
+    title: 'Eva',
+    description: 'Shoulder bag - lamb nappa leather',
+    color: 'Black',
+    for: 'Women',
+    status: 'New',
+    price: 1020,
+    availableColor: ['black', 'blush'],
+    isFavorite: false,
+    slug: 'eva',
+  },
+]
