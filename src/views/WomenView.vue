a98dac9a8318027f1104d8b207deddf0b33ad78f
Yusef Codes <youcefcopy@gmail.com>
2026-02-27 10:50:11 +0100

    rendering the women shoes in the women view

--- src/views/WomenView.vue
+++ src/views/WomenView.vue
@@ -1,10 +1,41 @@
+<script setup>
+// import FProduct from './FProduct.vue'
+// import shoes from '@/data/women-shoes.js'
+import FProduct from '@/components/FProduct.vue';
+import { productsStore } from '@/stores/productStore'
+
+// we have to set the store
+const myStore = productsStore()
+console.log(myStore.shoes)
+
+// import { ref, onMounted } from 'vue'
+
+// fetch data from an api
+// const array = ref([])
+// onMounted(async () => {
+//   try {
+//     const response = await fetch('https://fakestoreapi.com/products')
+//     const data = await response.json()
+//     array.value = data
+
+//     console.log(array.value)
+//   } catch (error) {
+//     console.log(error)
+//   }
+// })
+
+// search by name feature
+</script>
+
 <template>
-  <div class="women">This is the women products</div>
+  <div class="products women">
+    <FProduct v-for="shoe in myStore.activeFilter" :key="shoe.id" :product="shoe" />
+  </div>
 </template>
 
 <style scoped>
 .women {
-  min-height: 100dvh;
+  height: 100dvh;
   background-color: seagreen;
 }
 </style>
