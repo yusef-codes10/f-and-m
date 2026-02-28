620dd6bb7d5c4c2318570d09aabb93b83ea49ba1
Yusef Codes <youcefcopy@gmail.com>
2026-02-28 10:16:55 +0100

    best plan: is not live search for an e-commerce website

---
+++ src/views/SearchResultView.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+<template>
+    <div>
+        
+    </div>
+</template>
+<style scoped>
+    
+</style>
+
+<!-- TODO: the professional way is to render all the search results from the navbar to the new searchResilt view using router queries -->
