0493d36426b17e26e0a0e1d3a9a1b7ccd1bb983a
Yusef Codes <youcefcopy@gmail.com>
2026-02-28 11:20:22 +0100

    using the v-for to load all the filtered products is now working | all's left is styling

--- src/views/SearchResultView.vue
+++ src/views/SearchResultView.vue
@@ -1,7 +1,7 @@
 <script setup>
 // Access the search query from the route itself
-import { useRoute } from 'vue-router';
-import {computed} from 'vue'
+import { useRoute } from 'vue-router'
+import { computed } from 'vue'
 import { productsStore } from '@/stores/productStore'
 
 // we have to set the store
@@ -10,31 +10,35 @@ const myStore = productsStore()
 const route = useRoute()
 
 // better way to access the query is with computed
-const searchQuery = computed( () =>{
-    return route.query.q?.toLowerCase() || ''
+const searchQuery = computed(() => {
+  return route.query.q?.toLowerCase() || ''
 })
 
 // filterd products here
 const filterdProducts = computed(() => {
-    return myStore.allProducts.filter( product =>
-        product.title.toLowerCase().includes(searchQuery.value)
-    )
+  return myStore.allProducts.filter((product) =>
+    product.title.toLowerCase().includes(searchQuery.value)
+  )
 })
 
-console.log(myStore.allProducts.map(e => e.title));
+console.log(myStore.allProducts.map((e) => e.title))
 </script>
 <template>
-    <div class="search">
-        <p>Search results for {{searchQuery}}</p>
-        <div class="result" v-if="filterdProducts.length"></div>
-        <div class="not-found" v-else>Page not found</div>
+  <div class="search">
+    <p>Search results for {{ searchQuery }}</p>
+    <div class="result" v-if="filterdProducts.length">
+      <p v-for="product in filterdProducts" :key="product.id">
+        {{ product.title }}
+      </p>
     </div>
+    <div class="not-found" v-else>Page not found</div>
+  </div>
 </template>
 <style scoped>
-    .search {
-        height: 100vh;
-        background-color: aqua;
-    }
+.search {
+  height: 100vh;
+  background-color: aqua;
+}
 </style>
 
 <!-- TODO: the professional way is to render all the search results from the navbar to the new searchResilt view using router queries -->
