a5874c29fb7e6ca63e9420be0050a95d22d4637a
Yusef Codes <youcefcopy@gmail.com>
2026-02-28 10:43:17 +0100

    we have to access the search query from the route itself

--- src/views/SearchResultView.vue
+++ src/views/SearchResultView.vue
@@ -1,13 +1,16 @@
 <script setup>
-
+// Access the search query from the route itself
 </script>
 <template>
-    <div>
-        
+    <div class="search">
+        <p>Search results for {{  }}</p>
     </div>
 </template>
 <style scoped>
-    
+    .search {
+        height: 100vh;
+        background-color: aqua;
+    }
 </style>
 
 <!-- TODO: the professional way is to render all the search results from the navbar to the new searchResilt view using router queries -->
