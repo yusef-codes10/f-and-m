3eb5feb9ba820dbafaf66d53078f1427b57b0fb4
Yusef Codes <youcefcopy@gmail.com>
2026-03-01 12:40:21 +0100

    the accordion component

---
+++ src/components/AccordionComp.vue
@@ -0,0 +1,25 @@
+<script setup>
+import {ref} from 'vue'
+
+const isOpen = ref(false)
+
+const toggleIsOpen = () => {
+    isOpen.value = !isOpen.value
+}
+</script>
+<template>
+    <div>
+        <div class="btn">
+            <button @click="toggleIsOpen">
+                <i class="fa-solid fa-chevron-up"></i>
+                <i class="fa-solid fa-chevron-up"></i>
+
+            </button>
+        </div>
+        <div class="content" v-if="isOpen">
+            Hello dude
+        </div>
+    </div>
+</template>
+<style scoped>
+</style>
