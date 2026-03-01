ca26f880794133a7fb6b168d606357811af1d959
Yusef Codes <youcefcopy@gmail.com>
2026-03-01 12:58:41 +0100

    width and max-width combo | stretches to the parent width but do not become ridiculosuly large

--- src/components/AccordionComp.vue
+++ src/components/AccordionComp.vue
@@ -1,45 +1,40 @@
 <script setup>
-import {ref} from 'vue'
+import { ref } from 'vue'
 
 const isOpen = ref(false)
 
 const toggleIsOpen = () => {
-    isOpen.value = !isOpen.value
+  isOpen.value = !isOpen.value
 }
 </script>
 <template>
-    <div class="accordion">
-        <div class="accordion-header">
-            <div class="accordion-title">
-                Title
-            </div>
-            <button @click="toggleIsOpen">
-                <i :class="isOpen ? 'fa-solid fa-chevron-up' : 'fa-solid fa-chevron-down'"></i>
-
-            </button>
-        </div>
-        <div class="accordion-content" v-if="isOpen">
-            Hello dude
-        </div>
+  <div class="accordion">
+    <div class="accordion-header">
+      <div class="accordion-title">Title</div>
+      <button @click="toggleIsOpen">
+        <i :class="isOpen ? 'fa-solid fa-chevron-up' : 'fa-solid fa-chevron-down'"></i>
+      </button>
     </div>
+    <div class="accordion-content" v-if="isOpen">Hello dude</div>
+  </div>
 </template>
 <style scoped>
-
 .accordion {
-    height: auto;
-    width: 300px;
-    background-color: aqua;
+  width: 100%;
+  max-width: 400px; /* optional */
+  height: auto;
+  background-color: aqua;
 }
 
 .accordion-header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
 }
 
 .accordion-header button {
-    background: none;
-    border: none;
+  background: none;
+  border: none;
 }
 </style>
