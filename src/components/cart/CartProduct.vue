982a008f47b26839aaa31b96eeaff8dae6bbbeec
Yusef Codes <youcefcopy@gmail.com>
2026-03-06 10:05:08 +0100

    rendering the cart items from the cart store | I think we should make the cart array computed property

--- src/components/cart/CartProduct.vue
+++ src/components/cart/CartProduct.vue
@@ -1,8 +1,11 @@
 <script setup>
-            
+    import cartStore from '@/stores/cartStore'
+
+    const myCartStore = cartStore()
 </script>
 <template>
-    <div>
+    <div v-for="item in myCartStore" :key="item.product.id">
+        <h1>item.product.title</h1>
         
     </div>
 </template>
